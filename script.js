83821b9a06b5756455f58a59dca5435245e733c5
alinagunich2 <alinka.gunich9@gmail.com>
2025-02-03 18:26:53 +0300

    button

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+function gtag_report_conversion(url) {
+    var callback = function () {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-806323095/GGGYCJGeuYoDEJeHvoAD',
+        'event_callback': callback
+    });
+    return false;
+  }
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('menu');
     const menuList = menu.querySelector('.menu__list');
     const iconMenu = document.querySelector('.icon-menu');
     const body = document.body;
+    const buttonFirst = document.getElementById('button-first');
+    const buttonSecond = document.getElementById('button-second');
 
     iconMenu.addEventListener('click', () => {
         menuList.classList.toggle('active');
@@ -15,4 +32,18 @@ document.addEventListener('DOMContentLoaded', () => {
             body.classList.remove('body--menu-open');
         }
     });
+
+    if (buttonFirst) {
+        buttonFirst.addEventListener('click', () => {
+            gtag_report_conversion();
+        });
+    }
+
+
+    if (buttonSecond) {
+        buttonSecond.addEventListener('click', () => {
+            gtag_report_conversion();
+        });
+    }
 });
+
